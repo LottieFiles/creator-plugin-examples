b1e4741224a7ae32a85d14137497c8bc1edab901
jen <63580680+limjenli@users.noreply.github.com>
2026-03-02 23:17:43 +0800

    fix: eslint

--- basic-plugin-react-with-ui-lib/eslint.config.js
+++ basic-plugin-react-with-ui-lib/eslint.config.js
@@ -24,7 +24,11 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        project: './tsconfig.json',
+        project: [
+          './tsconfig.app.json',
+          './tsconfig.node.json',
+          './tsconfig.plugin.json',
+        ],
         tsconfigRootDir: __dirname,
       },
     },
